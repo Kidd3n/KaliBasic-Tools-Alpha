2395436d71c7f928f11503ba5dda0c4d8f1c8ae3
Ruben Gomez <santiagogomezgarro@gmail.com>
2022-12-27 21:35:42 -0600

    Se agrega un pequeno panel

--- KaliBasic-Tools.sh
+++ KaliBasic-Tools.sh
@@ -5,21 +5,29 @@ if [ $(id -u) -ne 0 ]; then
 exit 1
 fi
 
+	echo "Comprobando Dependencias..."
+	sudo apt install toilet > /dev/null 2>&1
+	sleep 7
+	toilet -f term -F border:metal KaliBasic-Tools by Rubelmo
+
 read -p "Empezara todo el proceso y durara mucho tiempo (NO ES RECOMENDABLE REINICIAR NI APAGAR TU PC) Deseas continuar? [y/n]: " rps
 if [ $rps == "y" ]; then 
-	apt update && apt upgrade -y
- 	apt install gdebi
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	dpkg -i google-chrome-stable_current_amd64.deb
-	sudo rm -r google-chrome-stable_current_amd64.deb
-	git clone https://github.com/t3l3machus/Villain.git
+	echo "Instalando Herramientas...Espere y no cierre el Script"
+	apt update > /dev/null 2>&1 && apt upgrade -y > /dev/null 2>&1
+ 	apt install gdebi > /dev/null 2>&1
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+	dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+	sudo rm -r google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+	git clone https://github.com/t3l3machus/Villain.git > /dev/null 2>&1
 	cd ./Villain
-	pip3 install -r requirements.txt
-	sudo apt install mdk3 -y
-	wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-	dpkg -i code_1.74.2-1671533413_amd64.deb
-	sudo rm -r code_1.74.2-1671533413_amd64.deb
-	sudo apt install maltego -y
+	pip3 install -r requirements.txt > /dev/null 2>&1
+	sudo apt install mdk3 -y > /dev/null 2>&1
+	wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 > /dev/null 2>&1
+	dpkg -i code_1.74.2-1671533413_amd64.deb > /dev/null 2>&1
+	sudo rm -r code_1.74.2-1671533413_amd64.deb > /dev/null 2>&1
+	sudo apt install maltego -y > /dev/null 2>&1
+	echo "Se Termino el Script"
+
 elif [ $rps == "n" ]; then 
 	echo "Cerrando Script"
 fi
